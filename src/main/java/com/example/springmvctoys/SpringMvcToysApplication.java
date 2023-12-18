55effb8d71d6a5cec64ab3ec9f9aa11b8c7e6b8d
gwangsikkim <syberhiphop@gmail.com>
2023-12-18 21:01:10 +0900

    created init springboot

---
+++ src/main/java/com/example/springmvctoys/SpringMvcToysApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvctoys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcToysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcToysApplication.class, args);
+    }
+
+}
